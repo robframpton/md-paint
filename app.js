c504e393da3af845385ba1dc6fe4a94f793e1155
Robert-Frampton <ROBERT.FRAMPTON@LIFERAY.COM>
2016-05-11 16:20:43 -0700

    quit app when all windows are closed

--- app.js
+++ app.js
@@ -12,9 +12,7 @@ var argv = minimist(process.argv.slice(2));
 var mainWindow = null;
 
 app.on('window-all-closed', function() {
-	if (process.platform != 'darwin') {
-		app.quit();
-	}
+	app.quit();
 });
 
 app.on('ready', function() {
