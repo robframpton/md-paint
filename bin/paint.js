f0a2ccda6fc49316725bc630d42368108b485cc1
Robert-Frampton <ROBERT.FRAMPTON@LIFERAY.COM>
2016-08-19 09:20:48 -0700

    Add update-notifier

--- bin/paint.js
+++ bin/paint.js
@@ -2,6 +2,13 @@
 
 var mdPaint = require('../index.js');
 var minimist = require('minimist');
+var updateNotifier = require('update-notifier');
+
+var notifier = updateNotifier({
+	pkg: require('../package.json')
+});
+
+notifier.notify();
 
 var argv = minimist(process.argv.slice(2));
 
